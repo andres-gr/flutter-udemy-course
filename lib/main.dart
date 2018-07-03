d53077c2dc4156c531151661bc13c1509fa0058c
Andres Garcia <andres.garcia@bmtask.com>
2018-07-02 22:08:42 -0500

    section 2, lecture 29 complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,51 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Easy List'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _products.add('Advanced Food');
+                  });
+                },
+                child: Text('Add Product'),
+              ),
+            ),
+            Column(
+              children: _products
+                  .map(
+                    (elem) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/food.jpg'),
+                              Text(elem),
+                            ],
+                          ),
+                        ),
+                  )
+                  .toList(),
+            )
+          ],
         ),
       ),
     );
