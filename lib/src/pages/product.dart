c0108cefd26c4402f81d1ace119f8f85cd8deee0
Andres Garcia <andres.garcia@bmtask.com>
2018-07-08 17:30:37 -0500

    section 6, lecture 71 complete

--- lib/src/pages/product.dart
+++ lib/src/pages/product.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
@@ -11,31 +13,37 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Padding(
-            child: Text(title),
-            padding: const EdgeInsets.all(10.0),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: RaisedButton(
-              child: const Text('DELETE'),
-              color: Theme.of(context).accentColor,
-              onPressed: () => Navigator.pop(
-                    context,
-                    true
-                  ),
-              textColor: Colors.white,
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Padding(
+              child: Text(title),
+              padding: const EdgeInsets.all(10.0),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: RaisedButton(
+                child: const Text('DELETE'),
+                color: Theme.of(context).accentColor,
+                onPressed: () => Navigator.pop(context, true),
+                textColor: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
+      onWillPop: () {
+        Navigator.pop(
+          context,
+          false,
+        );
+        return Future.value(false);
+      },
     );
   }
 }
