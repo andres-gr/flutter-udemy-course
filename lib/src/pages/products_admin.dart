84678f6c0f956ffde2b9d2aa7e3b8ac4385f3904
Andres Garcia <andres.garcia@bmtask.com>
2018-07-09 20:00:03 -0500

    section 6, lecture 76 complete

--- lib/src/pages/products_admin.dart
+++ lib/src/pages/products_admin.dart
@@ -1,38 +1,58 @@
 import 'package:flutter/material.dart';
 
 import 'package:udemy_course/src/pages/products.dart';
+import 'package:udemy_course/src/pages/product_create.dart';
+import 'package:udemy_course/src/pages/product_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Products Admin'),
-      ),
-      body: Center(
-        child: const Text('Products'),
-      ),
-      drawer: Drawer(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              AppBar(
-                automaticallyImplyLeading: false,
-                title: const Text('Admin Products'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Products Admin'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: 'Create Product',
               ),
-              ListTile(
-                title: const Text('Products List'),
-                onTap: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => ProductsPage(),
-                    ),
-                  );
-                },
+              Tab(
+                icon: Icon(Icons.list),
+                text: 'My Products',
               ),
             ],
           ),
+        ),
+        drawer: Drawer(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: const Text('Admin Products'),
+                ),
+                ListTile(
+                  title: const Text('Products List'),
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => ProductsPage(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ProductCreatePage(),
+            ProductListPage(),
+          ],
         ),
       ),
     );
