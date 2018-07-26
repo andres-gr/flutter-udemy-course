b9a243322c0972d3d0cb1906102f96b8029d16c6
Andres Garcia <andres.garcia@bmtask.com>
2018-07-25 22:42:39 -0500

    section 8, lecture 100 complete

--- lib/src/products.dart
+++ lib/src/products.dart
@@ -14,7 +14,12 @@ class Products extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset(products[index]['imageUrl']),
-          Text(products[index]['title']),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 10.0,
+            ),
+            child: Text(products[index]['title']),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
